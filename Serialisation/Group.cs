dc53f679e71ff132b87826a56a35bed79e9dca11
anna Pfeifer <anna.pfeifer78@gmail.com>
2019-09-12 13:35:45 +0200

    Projektdateien hinzufügen.

---
+++ Serialisation/Group.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Serialisation
+{ 
+    [Serializable]
+    public class Group
+    {
+        private readonly Random rnd = new Random(DateTime.Now.Millisecond);
+        private int privateint;
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public Group()
+        {
+            Number = rnd.Next(1, 10);
+            Name = "Group" +rnd;
+        }
+        public Group(int number,string name)
+        {
+            if(number!=0 && name!=null)
+            {
+                Number = number;
+                Name = name;
+            }
+        }
+        public void SetPrivet(int i)
+        {
+            privateint = i;
+        }
+        public int GetPrivet()
+        {
+            return privateint;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Serialisation/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialisation
+{
+    //[Serializable]
+    [DataContract]
+
+    public class Student
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public Group Group { get; set; }
+        public Student(string name,int age)
+        {
+            if(name!=null && age!=0)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
